9a620a207244623514ca47088ccc986c62af1f18
黃郁珺 <72348732+yc811@users.noreply.github.com>
2025-09-09 18:48:04 +0800

    Update the script
    Merge 04_01.R and 04_02.R, update the whole scripts

--- 04_cross_correlation_01_correlation_scatter.R
+++ 04_cross_correlation_01_correlation_scatter.R
@@ -1,5 +1,4 @@
-#!/usr/bin/env Rscript
-# Rscript 04_cross_correlation_01_correlation_scatter.R -g expression_gene.txt -t expression_TE.txt 
+# Rscript 04_cross_correlation_01_correlation_scatter.R -g DEG.txt -t DETE.txt 
 
 start_time <- Sys.time()
 
@@ -7,14 +6,8 @@ library(optparse)
 library(ggplot2)
 library(ggpointdensity)
 library(viridis)
-
-#---- Option parser ----
-option_list = list(
-  make_option(c("-g", "--gene"), type="character", help="Gene expression file"),
-  make_option(c("-t", "--TE"), type="character", help="TE expression file")
-)
-opt_parser = OptionParser(option_list=option_list)
-opt = parse_args(opt_parser)
+library(reshape2)
+library(stringr)
 
 #---- Helper functions ----
 quadrant_counts <- function(df, xx, yy) {
@@ -34,13 +27,11 @@ lm_eqn_text <- function(df, yy, xx){
   return(eq)
 }
 
-
 plot_delta_scatter <- function(df, x, y, fname, title, xlab, ylab){
   counts <- quadrant_counts(df, x, y)
   eq_text <- paste0(lm_eqn_text(df, y, x))
   xlim <- max(abs(df[[x]]), na.rm=TRUE)
   ylim <- max(abs(df[[y]]), na.rm=TRUE)
-
 
   png(file=fname, width=3000, height=2500, res=400)
   p <- ggplot(df, aes_string(x=x, y=y)) +
@@ -66,41 +57,39 @@ plot_delta_scatter <- function(df, x, y, fname, title, xlab, ylab){
           plot.caption=element_text(size=10, hjust=0)) +
     labs(title=title, caption = eq_text, color="Number of\nneighboring\npoints\n ", x=xlab, y=ylab)
   print(p)
+
   dev.off()
+
 }
 
 #---- Read expression ----
-gene_exp <- read.table(opt$gene, header=TRUE, sep="\t", row.names=1)
-#gene_exp<-read.table("expression_gene.txt", header=T, sep="\t") 
-gene_exp <- gene_exp[rowSums(gene_exp) != 0, ]
-TE_exp   <- read.table(opt$TE, header=TRUE, sep="\t", row.names=1)
-#TE_exp<-read.table("expression_TE.txt", header=T, sep="\t") 
-TE_exp   <- TE_exp[rowSums(TE_exp) != 0, ]
+gene_exp<-read.table("DEG.txt", header=T, sep="\t") 
+TE_exp<-read.table("DETE.txt", header=T, sep="\t") 
 
-stages <- intersect(colnames(gene_exp), colnames(TE_exp))
-if(length(stages) < 2) stop("Need at least two common stages")
-
-# delta expression
-for(i in 1:(length(stages)-1)){
-  for(j in (i+1):length(stages)){
-    si <- stages[i]; sj <- stages[j]
-    gene_exp[[paste0("dexp_", si, "_", sj)]] <- log2((gene_exp[[si]]+0.1)/(gene_exp[[sj]]+0.1))
-    TE_exp[[paste0("dTEexp_", si, "_", sj)]] <- log2((TE_exp[[si]]+0.1)/(TE_exp[[sj]]+0.1))
-  }
-}
+colnames(gene_exp) <- gsub("^logFC_", "dexp_", colnames(gene_exp))
+colnames(TE_exp)   <- gsub("^logFC_", "dTEexp_", colnames(TE_exp))
+colnames(gene_exp) <- gsub("^PValue_", "PV_g_", colnames(gene_exp))
+colnames(TE_exp)   <- gsub("^PValue_", "PV_TE_", colnames(TE_exp))
+colnames(gene_exp) <- gsub("^FDR_", "FDR_g_", colnames(gene_exp))
+colnames(TE_exp)   <- gsub("^FDR_", "FDR_TE_", colnames(TE_exp))
 
 #---- Read methylation ----
 CG_TE  <- read.table("TE_CG_tab.txt", header=TRUE, sep="\t")
 CHG_TE <- read.table("TE_CHG_tab.txt", header=TRUE, sep="\t")
 CHH_TE <- read.table("TE_CHH_tab.txt", header=TRUE, sep="\t")
 
-for(i in 1:(length(stages)-1)){
-  for(j in (i+1):length(stages)){
-    si <- stages[i]; sj <- stages[j]
-    CG_TE[[paste0("dTECG_", si, "_", sj)]]   <- (CG_TE[[si]] - CG_TE[[sj]])*100
-    CHG_TE[[paste0("dTECHG_", si, "_", sj)]] <- (CHG_TE[[si]] - CHG_TE[[sj]])*100
-    CHH_TE[[paste0("dTECHH_", si, "_", sj)]] <- (CHH_TE[[si]] - CHH_TE[[sj]])*100
-  }
+# follow order of stages in delta expression files
+fc_cols <- grep("^dexp_", colnames(gene_exp), value=TRUE)
+stages_pairs <- str_match(fc_cols, "^dexp_([A-Z]+)_([A-Z]+)$")[,2:3]
+
+# calculate delta methylation
+for(k in seq_len(nrow(stages_pairs))){
+  si <- stages_pairs[k,1]
+  sj <- stages_pairs[k,2]
+  
+  CG_TE[[paste0("dTECG_", si, "_", sj)]]   <- (CG_TE[[si]] - CG_TE[[sj]]) * 100
+  CHG_TE[[paste0("dTECHG_", si, "_", sj)]] <- (CHG_TE[[si]] - CHG_TE[[sj]]) * 100
+  CHH_TE[[paste0("dTECHH_", si, "_", sj)]] <- (CHH_TE[[si]] - CHH_TE[[sj]]) * 100
 }
 
 #---- Merge with promoter overlaps ----
@@ -116,73 +105,145 @@ ins_CG  <- merge(ins4, CG_TE,  by.x="TE_id", by.y="ID")
 ins_CHG <- merge(ins4, CHG_TE, by.x="TE_id", by.y="ID")
 ins_CHH <- merge(ins4, CHH_TE, by.x="TE_id", by.y="ID")
 
-#---- Scatter plots ----
-for(i in 1:(length(stages)-1)){
-  for(j in (i+1):length(stages)){
-    si <- stages[i]; sj <- stages[j]
+#---- function for Q2/Q4 boxplots ----
+plot_gene_TE_box <- function(df_subset, mC_type, si, sj, mode="Q2"){
+  if(nrow(df_subset) == 0) return(NULL)
+  
+  # output gene-TE pair
+  out_tbl <- df_subset[,c("gene_id","TE_id")]
+  write.table(out_tbl,
+              file=paste0(mode,"_gene_TE_pairs_", mC_type, "_", si, "_", sj,".txt"),
+              sep="\t", row.names=FALSE, quote=FALSE)
+  
+  # expression
+  expr_mat <- df_subset[,c("gene_id", paste0(si,".x"), paste0(sj,".x"))]
+  expr_mat_long <- melt(expr_mat, id.vars="gene_id", variable.name="stage", value.name="expr")
+  expr_mat_long$expr <- log2(expr_mat_long$expr + 0.1)
+  expr_mat_long$stage <- gsub("\\.x$", "", expr_mat_long$stage)
+  expr_mat_long$stage <- factor(expr_mat_long$stage, levels=c(sj, si)) # stage2, stage1
+  
+  # methylation
+  meth_mat <- df_subset[,c("TE_id", si, sj)]
+  meth_mat_long <- melt(meth_mat, id.vars="TE_id", variable.name="stage", value.name="methyl")
+  meth_mat_long$methyl <- meth_mat_long$methyl * 100  
+  meth_mat_long$stage <- factor(meth_mat_long$stage, levels=c(sj, si))
+  
+  # scale factor for mapping methylation to expression y-axis
+  expr_range <- range(expr_mat_long$expr, na.rm=TRUE)
+  meth_range <- range(meth_mat_long$methyl, na.rm=TRUE)
+  scale_factor <- diff(expr_range) / diff(meth_range)
+  
+  # means
+  expr_means <- aggregate(expr ~ stage, expr_mat_long, mean)
+  meth_means <- aggregate(methyl ~ stage, meth_mat_long, mean)
+  
+  # plot
+  p <- ggplot() +
+    geom_boxplot(data=expr_mat_long, aes(x=stage, y=expr),
+                 fill="#BFBFBF", width=0.35, position=position_nudge(x=-0.2)) +
+    geom_point(data=expr_means, aes(x=stage, y=expr), shape=18, size=6, color="black",
+               position=position_nudge(x=-0.2)) +
+    geom_text(data=expr_means, aes(x=stage, y=expr, label=sprintf("%.1f", expr)),
+              size=6, vjust=-1.2, color="black", position=position_nudge(x=-0.2)) +
+    geom_boxplot(data=meth_mat_long, aes(x=stage, y=methyl*scale_factor + expr_range[1] - meth_range[1]*scale_factor),
+                 fill="#E5C1AF", width=0.35, position=position_nudge(x=0.2)) +
+    geom_point(data=meth_means, aes(x=stage, y=methyl*scale_factor + expr_range[1] - meth_range[1]*scale_factor),
+               shape=18, size=6, color="black", position=position_nudge(x=0.2)) +
+    geom_text(data=meth_means, aes(x=stage, y=methyl*scale_factor + expr_range[1] - meth_range[1]*scale_factor,
+                                   label=sprintf("%.1f", methyl)),
+              size=6, vjust=-1.2, color="black", position=position_nudge(x=0.2)) +
+    scale_y_continuous(
+      name="Expression (log2 RPKM)",
+      sec.axis = sec_axis(~ (. - expr_range[1] + meth_range[1]*scale_factor)/scale_factor,
+                          name="Methylation level (%)")
+    ) +
+    scale_x_discrete(name="Stage") +
+    theme_bw() +
+    theme(axis.title=element_text(face="bold", size=20),
+          axis.text=element_text(face="bold", size=18),
+          axis.text.x=element_text(face="bold", size=18),
+          axis.text.y=element_text(face="bold", size=18))
+  
+  ggsave(filename=paste0(mode,"_boxplot_", mC_type, "_", si, "_", sj,".png"), p, width=6, height=5)
+}
 
-    # gene exp vs TE exp
-    plot_delta_scatter(ins4,
-      paste0("dexp_", si, "_", sj), paste0("dTEexp_", si, "_", sj),
+#---- Scatter plots + Q2/Q4 boxplots ----
+for(k in seq_along(fc_cols)){
+  x_col <- fc_cols[k]
+  si <- stages_pairs[k,1]
+  sj <- stages_pairs[k,2]
+  pv_col <- paste0("PV_g_", si, "_", sj)
+
+  # gene exp vs TE exp
+  df_sub <- subset(ins4,
+    abs(ins4[[x_col]]) > 1 & ins4[[pv_col]] < 0.05
+  )
+  if(nrow(df_sub) > 0){
+    plot_delta_scatter(df_sub,
+      x_col, paste0("dTEexp_", si, "_", sj),
       paste0("geneexp_TEexp_change_", si, "_", sj, "_scatter.png"),
       paste0("TE and gene expression changes\nbetween ", si, " and ", sj),
       expression(Delta~"Gene expression level (log2 RPKM FC)"),
       expression(Delta~"TE expression level (log2 RPKM FC)")
     )
+  }
 
-    # gene exp vs TE mC
-    plot_delta_scatter(ins_CG,
-      paste0("dexp_", si, "_", sj), paste0("dTECG_", si, "_", sj),
-      paste0("geneexp_TEmCG_change_", si, "_", sj, "_scatter.png"),
-      paste0("TE mCG and gene expression changes\nbetween ", si, " and ", sj),
-      expression(Delta~"Gene expression level (log2 RPKM FC)"),
-      expression(Delta~"TE CG methylation level (%)")
+  # gene exp vs TE mC + Q2/Q4
+  for(mC_type in c("CG","CHG","CHH")){
+    df_TE <- get(paste0("ins_", mC_type))
+    y_col <- paste0("dTE", mC_type, "_", si, "_", sj)
+    df_sub <- subset(df_TE,
+      abs(df_TE[[x_col]]) > 1 & df_TE[[pv_col]] < 0.05
     )
+    if(nrow(df_sub) > 0){
+      # scatter plot
+      plot_delta_scatter(df_sub,
+        x_col, y_col,
+        paste0("geneexp_TEm", mC_type, "_change_", si, "_", sj, "_scatter.png"),
+        paste0("TE m", mC_type, " and gene expression changes\nbetween ", si, " and ", sj),
+        expression(Delta~"Gene expression level (log2 RPKM FC)"),
+        expression(Delta~paste("TE", mC_type, "methylation level (%)"))
+      )
+      # Q2 boxplot
+      df_Q2 <- subset(df_TE,
+        abs(df_TE[[x_col]]) > 1 &
+        df_TE[[pv_col]] < 0.05 &
+        df_TE[[x_col]] < 0 &
+        df_TE[[y_col]] > 0
+      )
+      plot_gene_TE_box(df_Q2, mC_type, si, sj, mode="Q2")
+      # Q4 boxplot
+      df_Q4 <- subset(df_TE,
+        abs(df_TE[[x_col]]) > 1 &
+        df_TE[[pv_col]] < 0.05 &
+        df_TE[[x_col]] > 0 &
+        df_TE[[y_col]] < 0
+      )
+      plot_gene_TE_box(df_Q4, mC_type, si, sj, mode="Q4")
+    }
+  }
 
-    plot_delta_scatter(ins_CHG,
-      paste0("dexp_", si, "_", sj), paste0("dTECHG_", si, "_", sj),
-      paste0("geneexp_TEmCHG_change_", si, "_", sj, "_scatter.png"),
-      paste0("TE mCHG and gene expression changes\nbetween ", si, " and ", sj),
-      expression(Delta~"Gene expression level (log2 RPKM FC)"),
-      expression(Delta~"TE CHG methylation level (%)")
-    )
+  # TE exp vs TE mC
+  for(mC_type in c("CG","CHG","CHH")){
+    df_TE <- get(paste0("ins_", mC_type))
+    x_TE_col <- paste0("dTEexp_", si, "_", sj)
+    y_TE_col <- paste0("dTE", mC_type, "_", si, "_", sj)
+    pv_TE_col <- paste0("PV_TE_", si, "_", sj)
 
-    plot_delta_scatter(ins_CHH,
-      paste0("dexp_", si, "_", sj), paste0("dTECHH_", si, "_", sj),
-      paste0("geneexp_TEmCHH_change_", si, "_", sj, "_scatter.png"),
-      paste0("TE mCHH and gene expression changes\nbetween ", si, " and ", sj),
-      expression(Delta~"Gene expression level (log2 RPKM FC)"),
-      expression(Delta~"TE CHH methylation level (%)")
+    df_sub <- subset(df_TE,
+      abs(df_TE[[x_TE_col]]) > 1 & df_TE[[pv_TE_col]] < 0.05
     )
-
-    # TE exp vs TE mC
-    plot_delta_scatter(ins_CG,
-      paste0("dTEexp_", si, "_", sj), paste0("dTECG_", si, "_", sj),
-      paste0("TEexp_TEmCG_change_", si, "_", sj, "_scatter.png"),
-      paste0("TE mCG and TE expression changes\nbetween ", si, " and ", sj),
-      expression(Delta~"TE expression level (log2 RPKM FC)"),
-      expression(Delta~"TE CG methylation level (%)")
-    )
-
-    plot_delta_scatter(ins_CHG,
-      paste0("dTEexp_", si, "_", sj), paste0("dTECHG_", si, "_", sj),
-      paste0("TEexp_TEmCHG_change_", si, "_", sj, "_scatter.png"),
-      paste0("TE mCHG and TE expression changes\nbetween ", si, " and ", sj),
-      expression(Delta~"TE expression level (log2 RPKM FC)"),
-      expression(Delta~"TE CHG methylation level (%)")
-    )
-
-    plot_delta_scatter(ins_CHH,
-      paste0("dTEexp_", si, "_", sj), paste0("dTECHH_", si, "_", sj),
-      paste0("TEexp_TEmCHH_change_", si, "_", sj, "_scatter.png"),
-      paste0("TE mCHH and TE expression changes\nbetween ", si, " and ", sj),
-      expression(Delta~"TE expression level (log2 RPKM FC)"),
-      expression(Delta~"TE CHH methylation level (%)")
-    )
+    if(nrow(df_sub) > 0){
+      plot_delta_scatter(df_sub,
+        x_TE_col, y_TE_col,
+        paste0("TEexp_TEm", mC_type, "_change_", si, "_", sj, "_scatter.png"),
+        paste0("TE m", mC_type, " and TE expression changes\nbetween ", si, " and ", sj),
+        expression(Delta~"TE expression level (log2 RPKM FC)"),
+        expression(Delta~paste("TE", mC_type, "methylation level (%)"))
+      )
+    }
   }
 }
 
-
 end_time <- Sys.time()
 print(end_time-start_time)
-
