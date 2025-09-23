667df7d508eb50a3861bacfa9e0229414fb7e27d
黃郁珺 <72348732+yc811@users.noreply.github.com>
2025-09-23 18:46:38 +0800

    Update script

--- 1_TE_distribution.sh
+++ 1_TE_distribution.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# 02_insertion_02_genomic_features.sh
+# 1_TE_distribution.sh
 # Usage example:
-# bash 02_insertion_02_genomic_features.sh \
+# bash 1_TE_distribution.sh \
 #   -g gene.bed -c CDS.bed -5 5UTR.bed -e exon.bed -3 3UTR.bed \
-#   -p promoter.bed -t TE.bed -n genome.bed 
+#   -p promoter.bed -t TE.bed -fai genome.fa.fai 
 
 while [[ $# -gt 0 ]]; do
   case $1 in
@@ -14,14 +14,14 @@ while [[ $# -gt 0 ]]; do
     -3) utr3=$2; shift 2;;
     -p) promoter=$2; shift 2;;
     -t) te=$2; shift 2;;
-    -n) genome=$2; shift 2;;
+    -fai) faidx=$2; shift 2;;
     *) echo "Unknown option $1"; exit 1;;
   esac
 done
 
 # 必要參數檢查
-if [[ -z $gene || -z $cds || -z $utr5 || -z $exon || -z $utr3 || -z $promoter || -z $te || -z $genome  ]]; then
-  echo "Usage: bash 02_insertion_02_genomic_features.sh -g gene.bed -c CDS.bed -5 5UTR.bed -e exon.bed -3 3UTR.bed -p promoter.bed -t TE.bed -n genome.bed"
+if [[ -z $gene || -z $cds || -z $utr5 || -z $exon || -z $utr3 || -z $promoter || -z $te || -z $faidx  ]]; then
+  echo "Usage: bash 1_TE_distribution.sh -g gene.bed -c CDS.bed -5 5UTR.bed -e exon.bed -3 3UTR.bed -p promoter.bed -t TE.bed -fai genome.fa.fai"
   exit 1
 fi
 
@@ -40,8 +40,10 @@ sort -k1,1 -k2,2n -k3,3n intron.bed > intron_sort.bed
 # IGR
 bedtools merge -i gene_sort.bed > merge_2strands_gene.bed
 
-bedtools subtract -a "$genome" -b merge_2strands_gene.bed > IGR_nostrand.bed
-sort -k1,1 -k2,2n -k3,3n IGR_nostrand.bed > IGR_nostrand_sort.bed
+awk '{print $1"\t0\t"$2}' "$faidx" > genome.bed
+
+bedtools subtract -a genome.bed -b merge_2strands_gene.bed > IGR.bed
+sort -k1,1 -k2,2n -k3,3n IGR.bed > IGR_sort.bed
 
 # Merge with no strand info
 bedtools merge -i CDS_sort.bed > merge_2strands_CDS.bed
@@ -52,7 +54,7 @@ bedtools merge -i promoter_sort.bed > merge_2strands_promoter.bed
 bedtools merge -i TE_sort.bed > merge_2strands_TE.bed
 
 bedtools merge -i intron_sort.bed > merge_2strands_intron.bed
-bedtools merge -i IGR_nostrand_sort.bed > merge_2strands_IGR.bed
+bedtools merge -i IGR_sort.bed > merge_2strands_IGR.bed
 
 # Intersect TE with regions
 for region in gene CDS 5UTR exon intron 3UTR promoter IGR; do
@@ -61,10 +63,15 @@ done
 
 #------------------
 # Call R script
-Rscript 02_insertion_02_genomic_features.R \
+Rscript 1_TE_distribution.R \
   TE_on_gene.bed TE_on_CDS.bed TE_on_5UTR.bed TE_on_exon.bed TE_on_intron.bed TE_on_3UTR.bed TE_on_promoter.bed TE_on_IGR.bed \
   merge_2strands_TE.bed merge_2strands_gene.bed merge_2strands_CDS.bed merge_2strands_5UTR.bed merge_2strands_exon.bed \
-  merge_2strands_intron.bed merge_2strands_3UTR.bed merge_2strands_promoter.bed merge_2strands_IGR.bed "$genome" 
+  merge_2strands_intron.bed merge_2strands_3UTR.bed merge_2strands_promoter.bed merge_2strands_IGR.bed genome.bed 
+
+
+rm *_sort.bed
+rm merge_2strands_*bed
+rm TE_on_*bed
 
 
 
